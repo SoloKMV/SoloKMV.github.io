6b2532016fa7edfa63c37657c9f8569177ced3c0
SoloKMV <headshotix7@gmail.com>
2024-05-05 23:15:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -82,7 +82,9 @@ btn6.addEventListener("click", function(){
 	}
 });
 
-
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+	tg.sendData(item);
+});
 
 let usercard = document.getElementById("usercard");
 
